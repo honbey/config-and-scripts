642327e89b879e88bf000fdbb5d93fab14f18f64
honbey <honbey@qq.com>
2020-12-01 13:05:11 +0800

    Modified some files   1. Modified and tested 'fedora_wsl_install_opencv.sh'   2. Add some configuration to '.bashrc'

--- wsl/fedora_wsl_install_opencv.sh
+++ wsl/fedora_wsl_install_opencv.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 # 用于在 Fedora Remix for WSL 上编译安装 OpenCV 4.0
 set -e
+
 # 1. 安装依赖
-sudo dnf install python-devel
-sudo dnf install ncurses-devel nasm yasm
-sudo dnf install gtk+ gtk+-devel gtk2 gtk2-devel gtkglext-devel
-sudo dnf install libavc1394 libavc1394-devel libdc1394 libdc1394-devel
-sudo dnf install openjpeg-devel libjpeg-devel libpng-devel libtiff-devel
-  # 1.1 安装 wget 方便下载源码 
-sudo dnf wget
+sudo dnf install python-devel \
+    ncurses-devel nasm yasm \
+    gtk+ gtk+-devel gtk2 gtk2-devel gtkglext-devel \
+    libavc1394 libavc1394-devel libdc1394 libdc1394-devel \
+    openjpeg-devel libjpeg-devel libpng-devel libtiff-devel \
+    wget # 1.1 安装 wget 方便下载源码
 
 # 2. 下载 OpenCV 和 FFmpeg 并解压
 cd
 wget https://github.com/opencv/opencv/archive/4.0.0.zip
 unzip 4.0.0.zip
-mv opencv-4.0.0 opencv
+mv opencv-4.0.0 opencv && rm 4.0.0.zip
 wget https://github.com/opencv/opencv_contrib/archive/4.0.0.zip
 unzip 4.0.0.zip
-mv opencv_contrib_4.0.0 opencv_contrib
+mv opencv_contrib-4.0.0 opencv_contrib && rm 4.0.0.zip
 wget https://ffmpeg.org/releases/ffmpeg-4.2.2.tar.bz2
-tar xvf ffmpeg-4.2.2.tar.bz2
+tar jxvf ffmpeg-4.2.2.tar.bz2 && rm ffmpeg-4.2.2.tar.bz2
 
 # 3. 编译 FFmpeg
-cd ffmpeg-n4.2.2
+cd ffmpeg-4.2.2
 ./configure --disable-static --enable-shared
 make && sudo make install
   # 3.1 链接 FFmpeg
@@ -50,3 +50,6 @@ sudo make install
 
   # 4.1 查看是否安装成功
 opencv_version
+
+  # 4.2 清理文件
+#rm -rf ffmpeg-4.2.2/ opencv_contrib/ opencv
