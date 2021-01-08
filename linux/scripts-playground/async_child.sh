31b3b1f60f39c1455cbf8723d201a5a2e6445afd
honbey <honbey@honbey.com>
2021-01-08 22:32:33 +0800

    Added some linux scripts and moved some files

---
+++ linux/scripts-playground/async_child.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Asynchronous execution demo (child)
+echo "Child: child is running"
+sleep 5
+echo "Child: child is done. Exiting."
---
+++ linux/scripts-playground/while_read_file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# while read file
+while read distros version release; do
+    printf "Distro: %s\tVersion: %s\tReleased: %s\n" \
+        $distros \
+        $version \
+        $release
+done < distros.txt
